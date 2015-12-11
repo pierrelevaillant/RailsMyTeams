3a5bea0837beb39fc5ce2cbaa00084e398a4216c
Ravichandrane <rajendran.ravichandrane@gmail.com>
2015-12-11 16:30:47 +0100

    admin page secured

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,17 +1,7 @@
 class Admin::DashboardController < AdminController
 
-	before_filter :custom_manager, :only => [:index]
-
 	def index
 		@categories = Category.all
-	end
-
-	private
-	def custom_manager
-		authenticate_user!
-		unless current_user.superadmin
-			redirect_to root_url
-		end
 	end
 
 end
