80344d565fc8cd25efe78f80d35b754c95e186d8
Ravichandrane <rajendran.ravichandrane@gmail.com>
2015-12-11 16:07:40 +0100

    if user isn't admin redirect to home page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,17 @@
 class Admin::DashboardController < AdminController
+
+	before_filter :custom_manager, :only => [:index]
+
 	def index
 		@categories = Category.all
 	end
+
+	private
+	def custom_manager
+		authenticate_user!
+		unless current_user.superadmin
+			redirect_to root_url
+		end
+	end
+
 end
